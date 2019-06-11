8c079531f6bf9174b6950655e7495aaaa9fedade
Heramba <herambadash94@gmail.com>
2019-06-12 01:20:49 +0530

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+
+	return [
+		{title: 'No Scrubs', duration: '4:05' },
+		{title: 'Macarena', duration: '4:00'},
+		{title: 'All Star', duration: '3:45'},
+		{title: 'I Like It That Way', duration: '4:40'}
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
+
+export default combineReducers({
+
+	songs: songsReducer,
+	selectedSong: selectedSongReducer
+});
